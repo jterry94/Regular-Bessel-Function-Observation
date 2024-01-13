b4c8903c87bc8a4f30c89827155a2870025d1463
jterry94 <terryj@iit.edu>
2024-01-13 17:26:03 -0600

    Initial Commit

---
+++ Regular-Bessel-Function-Observation/Regular_Bessel_Function_ObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Regular_Bessel_Function_ObservationApp.swift
+//  Regular-Bessel-Function-Observation
+//
+//  Created by Jeff_Terry on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Regular_Bessel_Function_ObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
