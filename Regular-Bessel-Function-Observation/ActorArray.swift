b061030ff84353612dc593e69381a23db9d9352c
jterry94 <terryj@iit.edu>
2024-01-14 11:32:58 -0600

    First commit

---
+++ Regular-Bessel-Function-Observation/ActorArray.swift
@@ -0,0 +1,8 @@
+//
+//  ActorArray.swift
+//  Regular-Bessel-Function-Observation
+//
+//  Created by Jeff_Terry on 1/14/24.
+//
+
+import Foundation
---
+++ Regular-Bessel-Function-Observation/Operators.swift
@@ -0,0 +1,8 @@
+//
+//  Operators.swift
+//  Regular-Bessel-Function-Observation
+//
+//  Created by Jeff_Terry on 1/13/24.
+//
+
+import Foundation
